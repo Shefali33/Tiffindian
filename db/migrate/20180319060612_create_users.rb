fed75b19ccb45c3b6b3daef2f5d61a9ce74d54e1
Shefali33 <shefalibhasin3@gmail.com>
2018-03-19 13:18:54 +0530

    hjhh

--- db/migrate/20180319060612_create_users.rb
+++ db/migrate/20180319060612_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+    	t.integer :role_id
+    	t.string :name
+    	t.integer :contact_number
+    	t.string :E_mail
 
       t.timestamps
     end
