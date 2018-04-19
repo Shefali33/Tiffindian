59afa083c2a658165c3e7a27883000ac459315f1
amarjeet98 <aj60200@gmail.com>
2018-04-19 15:49:36 +0530

    menus

--- db/migrate/20180411112032_create_memberships.rb
+++ db/migrate/20180411112032_create_memberships.rb
@@ -1,10 +1,13 @@
 class CreateMemberships < ActiveRecord::Migration[5.1]
   def change
     create_table :memberships do |t|
-    	t.references :user
+    	t.integer :user_id
+    	t.integer :supplier_id
     	t.references :master_subscription
-    	t.time :start_time
-    	t.time :end_time
+    	t.date :start_date
+    	t.date :end_date
+    	t.integer :quantity_provided
+    	t.integer :quantity_left
       t.timestamps
     end
   end
