88bfa15f99c9a6118ea012aa9b020e223b7bf9ce
amarjeet98 <aj60200@gmail.com>
2018-04-24 12:40:01 +0530

    order_controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,4 +4,16 @@ class OrderController < ApplicationController
   def index
     @orders = Order.all
   end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order)permit(:user_id, :supplier_id,:ordered_date,:delivery_date,:delivery_time,:quantity,:price,:rating,:menu_id,:master_slot_id,:master_order_status_id,:master_order_type_id)
+    end
 end
